ec6c6f83ab36bf37f175069870cc765856dbfdab
jeffrey82221 <jeffrey82221@gmail.com>
2021-06-13 12:55:35 +0800

    add modification to PipeConfigBuilder in common/ETLBase.py

--- common/ETLBase.py
+++ common/ETLBase.py
@@ -6,7 +6,9 @@ import pandas as pd
 import numpy as np
 from pyflow import GraphBuilder
 
-class PipeConfigBuilder():
+from common.utils import Str2CodeAdaptor
+
+class PipeConfigBuilder(Str2CodeAdaptor):
     def __init__(self):
         self.pyflow_GB = GraphBuilder()
     def add(self, var_name, value, rank=None, color='gray', shape='cylinder', fontsize=None):
@@ -27,14 +29,15 @@ class PipeConfigBuilder():
                           )
         # no call function here, directly call the method in add. 
         pf_output = self.pyflow_GB()
-        process_module = DataNode(
+        config_module = DataNode(
             current_process_name,
             []
         )
-        process_module.set_process(current_process)
-        process_module.set_n_out(n_out)
-        process_module.set_pf_output_node(pf_output)
-        return process_module
+        config_module.set_process(current_process)
+        config_module.set_n_out(n_out)
+        config_module.set_pf_output_node(pf_output)
+        return config_module
+    
     def view(self, *args, **kargs):
         return self.pyflow_GB.view(
             *args, 
@@ -42,6 +45,16 @@ class PipeConfigBuilder():
         )
     def view_dependency(self, *args, **kargs):
         return self.pyflow_GB.view_dependency(*args, **kargs)
+    
+    def setups(self, obj = None, **kargs):
+        for var_name, value in kargs.items():
+            config_module = self.add(var_name, value)
+            if obj == 'global':
+                Str2CodeAdaptor.set_global_var(var_name, config_module)
+            elif obj:
+                Str2CodeAdaptor.add_var_to_obj(obj, var_name, config_module)
+            else:
+                Str2CodeAdaptor.add_var_to_obj(self, var_name, config_module)
     
 class PipelineBuilder():
     def __init__(self, pipe=None):
--- common/utils.py
+++ common/utils.py
@@ -13,3 +13,36 @@ def blockPrinting(func):
         return value
 
     return func_wrapper
+
+class Str2CodeAdaptor:
+    '''
+    By inherenting this class, we will be able to :  
+    
+      1. insert public and private variables into a class by assigning variable and experssion 
+      2. add variable into the top-level global variable pool. 
+      
+    '''
+    @staticmethod 
+    def set_global_var(var, value):
+        assert type(var) == str 
+        exec('Str2CodeAdaptor.tmp = value')
+        exec(f'{var}=Str2CodeAdaptor.tmp', globals())
+        del Str2CodeAdaptor.tmp 
+    @staticmethod
+    def add_var_to_obj(obj, var, value, declare='public'):
+        assert declare=='public' or declare == 'private'
+        assert type(var) == str
+        tmp = value
+        if declare == 'public':
+            exec(f'obj.{var}=tmp')
+        else:
+            exec(f'obj._{var}=tmp')
+    def set_var(self, var, value, declare='public'):
+        assert declare=='public' or declare == 'private'
+        assert type(var) == str 
+        tmp = value
+        if declare == 'public':
+            exec(f'self.{var}=tmp')
+        else:
+            exec(f'self._{var}=tmp')
+    
